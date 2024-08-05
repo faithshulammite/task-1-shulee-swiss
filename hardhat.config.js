9e2c3f2a1596c4d5b5e639f4b2feff0d4d73da27
Faithshulammite <82059278+faithshulammite@users.noreply.github.com>
2024-08-05 13:32:38 +0000

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,18 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0x0405c0585099bc72bbedfaf9b6a225fa12ff77d4feb89bd8f9a0398a52ae7974"], //Your private key starting with "0x"
-    },
-  },
-};
-require("@nomicfoundation/hardhat-toolbox");
- 
-module.exports = {
-  solidity: "0.8.19",
-  networks: {
-    swisstronik: {
-      url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0x"], //Your private key starting with "0x"
+      accounts: [""],
     },
   },
 };
